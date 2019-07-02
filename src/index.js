ec2f038b6e66f7812c43148e00dfa2d31652c783
HuyCam <camhuy165@gmail.com>
2019-07-02 15:15:49 -0500

    add log out feature

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // import model
 const User = require('./models/user');
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Server is live');
@@ -34,6 +34,7 @@ app.post('/users/register', async (req, res) => {
 
 app.post('/users/login', async (req, res) => {
     try {
+        // find and verify that user using his/her email and password and generate user token
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.generateToken();
         res.send({ user, token });
@@ -42,8 +43,31 @@ app.post('/users/login', async (req, res) => {
     }
 })
 
+app.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(doc => doc.token !== req.token);
+        await req.user.save();
+        res.send({ msg: 'Log out successfully. '});
+    } catch(e) {
+        res.status(500).send();
+    } 
+})
+
+// get user profile
 app.get('/users/me', auth, async(req, res) => {
     res.send({ user: req.user })
+})
+
+// logout all tokens
+app.post('/users/logout-all', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+
+        res.send({ msg: 'Log out all successfully'});
+    } catch(e) {
+        res.status(400).send();
+    }
 })
 
 app.listen(port, () => {
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,10 +3,9 @@ const User = require('../models/user');
 
 const auth = async (req, res, next) => {
     try {
+        // extract token from header to get user id
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-        console.log(decoded);
-    
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
     
         if (!user) {
